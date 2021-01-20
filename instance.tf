1f3bcbbd01c9d45892444d535381d63354eddddd
Rogerio Reis <linux.rogerio@gmail.com>
2021-01-20 12:01:49 -0300

    terraform

---
+++ instance.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-2"
+  shared_credentials_file = "/root/.aws/credentials"
+  profile = "myawsuser"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-05edbb8e25e281608"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.my-key.key_name}" 
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
+}
+
+resource "aws_key_pair" "my-key" {
+  key_name = "my-key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+
+}
+
+#resource "aws_security_group" "allow_ssh" {
+#  name = "allow_ssh"
+#  ingress {
+#    from_port = 22
+#    to_port = 22
+#    protocol = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+ # }
+ # egress {
+ #   from_port = 0
+ #   to_port = 0
+ #   protocol = -1
+ #   cidr_blocks = ["0.0.0.0/0"]
+#}
+
+#}
+
+output "example_public_dns" {
+  value = "${aws_instance.example.public_dns}"
+}
---
+++ openstack.tf
@@ -0,0 +1,8 @@
+provider "openstack" {
+    user_name = "admin"
+    tenant_name = "admin" 
+    password = "admin" 
+    auth_url = "http://myauthurl:"
+    region = "RegionOne"
+}
+
